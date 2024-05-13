783bb91e5db73036190c51bfe89de80791ce79b7
andre-zj <anjiang@bbns.org>
2024-05-13 08:33:52 -0400

    Update accessAPI.py

--- accessAPI.py
+++ accessAPI.py
@@ -1,1 +1,17 @@
+import requests
+import yfinance as yf
+import pandas as pd
+#r = requests.get()
+
+msft = yf.Ticker("MSFT")
+#print(msft.info)
+
+stockslist = pd.read_csv('nasdaq_screener_1715521077293.csv')
+names = 'Symbol'#Column names (what it says in the first row of the desired columns)
+
+stocks = []
+for stock in names:
+    stocks.append(yf.Ticker(stock))
+
+print(stocks)
 
